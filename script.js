4b23a99f9c1986c23ac11780786e851ed391b51d
dhivyaan2003 <106645952+dhivyaan2003@users.noreply.github.com>
2023-03-19 23:01:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ $('window').on('scroll load',function(){
 $('#menu').removeClass('fa-times');
 $('header').removeClass('toggle');
 
+if($(window).sscrollTop()>0){
+    $('top').show()
+}else{
+    $('top').hide()
+}
+
+
+});
+
+$('a[href*="#"]').click(function(e){
+     e.preventDefault();
+     $('html,body').animate({
+           scrollTop:$($(this).attr('href')).offset().top,
+     },
+     500,
+     'linear'
+     );
 });
 
 
